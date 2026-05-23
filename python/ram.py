d45b277dd2f8cda9366b4b0b6ccce2408861da5f
njara <njaratinarakotoarindriana@gmail.com>
2026-05-23 15:04:33 +0300

    Creation de interface.py et creation de la fonction get_ram dans ram.py.Récuperation des données à partir de ram.sh

--- python/ram.py
+++ python/ram.py
@@ -1,6 +1,10 @@
 import subprocess
-result=subprocess.check_output(["bash","../modules/ram.sh"])
-ram =result.decode().strip()
+def get_ram():
+    result=subprocess.check_output(["bash","../modules/ram.sh"])
+    data=result.decode().strip()
+    totale,disponible,utilisee,cache,swap=data.split("|")
+    return totale,disponible,utilisee,cache,swap
+
 
 #result=subprocess.run(["bash","../modules/ram.sh"],capture_output=True,text=True)
 #print(result.stdout)
