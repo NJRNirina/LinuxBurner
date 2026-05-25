64c59574f5c2bc15b56d3666160860b4a568612d
njara <njaratinarakotoarindriana@gmail.com>
2026-05-25 10:15:00 +0300

    On continuation de la barre

--- python/interface.py
+++ python/interface.py
@@ -3,19 +3,16 @@ from ram import get_ram
 black_background="#1A1A1A"
 text_colors="#F0F0F0"
 second_fond="#242424"
-
+accent_blue = "#2979FF"
 window=Tk()
 window.title("LinuxBURNER")
 window.geometry("1280x800")
 window.minsize(900,600)
 window.config(background=black_background)
 
-#menu superieure
-frame_menu=Frame(window,height=100,width=400,bg=second_fond)
-frame_menu.pack(fill=X)
-
-ram_frame=Frame(frame_menu,height=100,width=100,bg="white")
-ram_button=Button(ram_frame,text="RAM",bg=second_fond,fg=text_colors)
-ram_frame.grid(row=0,column=0,sticky="nsnew")
-ram_button.place(relx=0.25,rely=0.25)
+#barre de navigation
+nav_bar=Frame(window,height=50,bg=second_fond)
+nav_bar.pack(fill=X)
+nav_bar.pack_propagate(False)
+#logo de la barre de navigation
 window.mainloop()
