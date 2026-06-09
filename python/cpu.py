3c9902358ee361e2a63aff6b059f4b7fb51fa6d1
andih <andinyaina2@gmail.com>
2026-06-09 16:51:31 +0300

    modif again

--- python/cpu.py
+++ python/cpu.py
@@ -1,24 +1,7 @@
-import subprocess
+import subprocess 
 
-result = subprocess.run(
-    ["bash", "cpu.bash"],
-    capture_output=True,
-    text=True
-)
-
-lines = result.stdout.strip().splitlines()
-
-# Assign each echo line to a variable
-model       = lines[0]  
-core        = lines[1]   
-cpu_usage   = lines[2]   
-cpu_heat    = lines[3]   
-cmd_label   = lines[4]  
-cpu_per_cmd = lines[5:]  
-
-print(model)
-print(core)
-print(cpu_usage)
-print(cpu_heat)
-print(cmd_label)
-print(cpu_per_cmd)
+def get_cpu():
+    result=subprocess.check_output(["bash","../modules/cpu.bash"])
+    data=result.decode().strip()
+    Model,Core,Usage,Heat1,Heat2,Heat3,Heat4,Heat5,Heat6,processus=data.split("|")
+    return Model,Core,Usage,Heat1,Heat2,Heat3,Heat4,Heat5,Heat6,processus
