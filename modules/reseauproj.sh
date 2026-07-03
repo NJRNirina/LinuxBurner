2c0537aaab6354e33eda0ff774cc6e7cec9918e1
fyandrintsitohaina-png <fyandrintsitohaina@gmail.com>
2026-07-03 22:25:41 +0300

    mise en forme

--- modules/reseauproj.sh
+++ modules/reseauproj.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+JAUNE_VIF='\033[0;93m'
+BLEU_VIF='\033[0;94m'
+MAGENTA_VIF='\033[0;95m'
+CYAN_VIF='\033[0;96m'
+ROUGE_GRAS='\033[1;31m'
+VERT_GRAS='\033[1;32m'
+RESET='\033[0m'
+
 #Etape 1:identité de la machine
-echo  "ETAPE 1: IDENTITÉ DE LA MACHINE"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+echo -e  "\t\t${CYAN_VIF}ETAPE 1: IDENTITÉ DE LA MACHINE${RESET}"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
 echo ""
 
 #hostname=nom de la machine
 HOSTNAME=$(hostname)
-echo "nom de la machine:$HOSTNAME"
+echo -e "${BLEU_VIF}•Nom de la machine:${RESET}$HOSTNAME"
 
 #IP local (LAN) =IP publique
 IP_LOCALE=$(hostname -I | awk '{print $1}')
 if [ -z "$IP_LOCALE" ];then
 	IP_LOCALE="non connecte,impossible d'afficher l'ip locale."
 fi	
-echo "IP locale:$IP_LOCALE"
+echo -e "${BLEU_VIF}•IP locale:${RESET}$IP_LOCALE"
 #IP publique (WAN)
 if command -v curl &> /dev/null; then #-v veifie que la cmd existe
 	IP_PUBLIQUE=$(curl -s --max-time 10 ifconfig.me)
@@ -23,25 +33,30 @@ if command -v curl &> /dev/null; then #-v veifie que la cmd existe
 else
 	IP_PUBLIQUE="impossible de verifier l'IP publique:curl non installee,veuillez l'installer via sudo apt install curl."
 fi
-echo "IP publique:$IP_PUBLIQUE"
+echo -e "${BLEU_VIF}•IP publique:${RESET}$IP_PUBLIQUE"
 
 #adresse MAC
 MAC=$(ip a | grep 'link/ether' | awk '{print $2}' | head -1)
-echo "adresse MAC:$MAC"
+echo -e "${BLEU_VIF}•Adresse MAC:${RESET}$MAC"
 
 #inteface réseau active
-INTERFACE_ACTIVE=$(ip link show | awk -F': ' '{print $2}')
+INTERFACE_ACTIVE=$(ip link show | awk -F': ' '/^[0-9]+:/{print "\t-"$2}')
 if [ -z "$INTERFACE_ACTIVE" ];then
-	echo "interface active:aucune interface active."
+	echo -e "${BLEU_VIF}•Interface active:${RESET}aucune interface active."
 else	
-	echo "interface active:$INTERFACE_ACTIVE"
+	echo -e "${BLEU_VIF}•Interface active:${RESET}"
+	echo -e "${MAGENTA_VIF}$INTERFACE_ACTIVE${RESET}"
+	
 fi	
 
 echo ""
 
 
 #Etape 2:traffic réseau (octets reçus et envoyés)
-echo "ETAPE 2: TRAFFIC RESEAU (OCTETS REÇUS ET ENVOYÉS)"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+echo -e "\t\t${CYAN_VIF}ETAPE 2: TRAFFIC RESEAU (OCTETS REÇUS ET ENVOYÉS)${RESET}"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+
 echo ""
 
 tail -n +3 /proc/net/dev | while read -r ligne;do
@@ -53,38 +68,40 @@ TX_BYTES=$(echo "$STATS" | awk '{print $9}') #octets reçus
 #conversion en mo , 1mo =1 048 576 | bc=bash calculator
 RX_MO=$(echo "$RX_BYTES/1048576" | bc)
 TX_MO=$(echo "$TX_BYTES/1048576" | bc)
-echo "Interfaces actives: $INTERFACE"
-echo "Octets envoyés (en Mo): $RX_MO"
-echo "Octets reçus (en Mo): $TX_MO"
+echo -e "${BLEU_VIF}•Interfaces actives:${RESET} $INTERFACE"
+echo -e "${BLEU_VIF}•Octets envoyés (en Mo):${RESET} $RX_MO"
+echo -e "${BLEU_VIF}•Octets reçus (en Mo):${RESET} $TX_MO"
 done
 echo ""
 #bol afaka apina vitesse en temps reel
 
 #Etape 3:test connectivité
-echo "ETAPE 3: TEST CONNECTIVITÉ"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+echo -e "\t\t${CYAN_VIF}ETAPE 3: TEST CONNECTIVITÉ${RESET}"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+
 echo ""
 
 #test ping,DNS,latence
 
 #test connectivité vers google
-echo "Test connectivité vers google:"
-ping -c 3 8.8.8.8 >/dev/null 2>&1 #>/dev/null 2>&1 cache la sortie normale et les messages d erreurs
+echo -e "${BLEU_VIF}•Test connectivité vers google:${RESET}"
+ping -c 3 8.8.8.8 >/dev/null 2>&1
 PING_RESULT=$? #tsy napety anle teo an tonga d le echo no captureny f tsy le ping
-echo ""
 if [ $PING_RESULT -eq 0 ] ;then #$? code de retour en bash,0=succès,1=échec
-	echo "Internet accessible."
+	echo -e "\t${VERT_GRAS}→ Internet accessible.${RESET} "
 else
-	echo "Pas d'accès à internet"	
+	echo -e "\t${ROUGE_GRAS}→ Pas d'accès à internet.${RESET}"	
 fi
 
 #test DNS
 echo ""
-echo "Test DNS:"
+echo -e "${BLEU_VIF}•Test DNS:${RESET}"
 DNS=$(nslookup google.com | grep -A1 'Name:' | grep 'Address:' | head -1 | awk '{print $2}') #-A:affiche une ligne après celle que l'on atrouvé (After)
 if [ -n "$DNS" ]; then #-n vérifie si la chaine n'est pas vide
-	echo "DNS fonctionne."
+	echo -e  "\t${VERT_GRAS}→ DNS fonctionne.${RESET}"
 else
-	echo "DNS ne fonctionne pas! Sites web inaccessibles."
+	echo -e "\t${ROUGE_GRAS}→ DNS ne fonctionne pas! Sites web inaccessibles.${RESET}"
 fi
 echo ""
 
@@ -99,45 +116,49 @@ PING2=$?
 if [ $PING2 -eq 0 ] ;then
 	LATENCE=$(echo "$RESULT" | tail -1 | awk -F'/' '{print $5}')
 	if [ $(echo "$LATENCE < 50") ]; then
-		echo "Connexion rapide."
+		echo -e "${VERT_GRAS}---> Connexion rapide.${RESET}"
 	elif [ $(echo "$LATENCE > 150") ]; then
-		echo "Connexion normale."
+		echo -e "${JAUNE_VIF}---> Connexion normale.${RESET}"
 	else
-		echo "Connexion lente."
+		echo -e "${ROUGE_GRAS}---> Connexion lente.${RESET}"
 	fi
 else
-	echo "Pas d'accès à google.com"
+	echo -e "${ROUGE_GRAS}---> Pas d'accès à google.com.${RESET}"
 fi
 echo ""
 
 #étape 4:services en écoutes (ports et connexions)
-echo "ETAPE 4: SERVICES EN ÉCOUTE (PORTS OUVERTS ET CONNEXIONS ACTIVES)"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+echo -e "\t\t${CYAN_VIF}ETAPE 4: SERVICES EN ÉCOUTE (PORTS OUVERTS ET CONNEXIONS ACTIVES)${RESET}"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
 echo ""
 #ports en écoute
 #ss -tulp
 
-LISTE_DE_PORTS_EN_ECOUTE=$(ss -tulp | awk '{n=split($5, a, ":"); print a[n]}')
-echo "-Liste des ports en écoute:$LISTE_DE_PORTS_EN_ECOUTE"
+LISTE_DE_PORTS_EN_ECOUTE=$(ss -tulp | awk 'NR>1{n=split($5, a, ":"); print "\t-"a[n]}')
+echo -e "${BLEU_VIF}•Liste des ports en écoute:${RESET}"
+echo -e "$LISTE_DE_PORTS_EN_ECOUTE"
 echo ""
 NB_DE_PORTS_EN_ECOUTE=$(ss -tuln | awk '{n=split($5, a, ":"); print a[n]}' | wc -l)
-echo "-Le nombre de ports en ecoute:$NB_DE_PORTS_EN_ECOUTE"
+echo -e "${BLEU_VIF}•Le nombre de ports en ecoute:${RESET}${JAUNE_VIF}$NB_DE_PORTS_EN_ECOUTE${RESET}"
 echo ""
 
 #connexions ouverts
-echo "Le nombre de connexions actives:"
-echo ""
+echo -e "${BLEU_VIF}•Le nombre de connexions actives:${RESET}"
 NB_ESTABLISHED=$(ss -tn | grep -c 'ESTAB') #-c compte le nb qui contient le mot
 NB_TIME_WAIT=$(ss -tn | grep -c 'TIME-WAIT')
 NB_CLOSE_WAIT=$(ss -tn | grep -c 'CLOSE-WAIT')
-echo "-Nombre de connexion active en cours:$NB_ESTABLISHED"
-echo "-Nombre de connexion en fermeture:$NB_TIME_WAIT"
-echo "-Nombre de connexion en attente de fermeture:$NB_CLOSE_WAIT"
+echo -e "\t${BLEU_VIF}-Nombre de connexion active en cours:${RESET}${JAUNE_VIF}$NB_ESTABLISHED${RESET}"
+echo -e "\t${BLEU_VIF}-Nombre de connexion en fermeture:${RESET}${JAUNE_VIF}$NB_TIME_WAIT${RESET}"
+echo -e "\t${BLEU_VIF}-Nombre de connexion en attente de fermeture:${RESET}${JAUNE_VIF}$NB_CLOSE_WAIT${RESET}"
 
 #commande (exemple:echo "texte") | tee -a fichier.log : affiche dans le terminalet sauvegarde en meme temps
 #fichier.log pour afficher en même dans le terminal et dans le fichier
 #date '%d/%m/%Y %H:%M:%S'
 echo ""
-echo "Date:$(date +"%d/%m/%Y %H:%M:%S")"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
+echo -e "\t${CYAN_VIF}Rapport du: Date:$(date +"%d/%m/%Y %H:%M:%S")${RESET}"
+echo -e "\t${CYAN_VIF}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
 
 
 
